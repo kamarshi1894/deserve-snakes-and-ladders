67d2902bfc6bb57a7cf33615d2965934f5779a75
pranit.k <pranit.kamarshi@cowrks.com>
2020-09-29 23:19:26 +0530

    moved random variable to object scope in FairDice and EvenBiasedDice

--- src/dice/EvenBiasedDice.java
+++ src/dice/EvenBiasedDice.java
@@ -4,18 +4,17 @@ import java.util.Random;
 
 public class EvenBiasedDice implements Dice {
     private final int numFaces;
+    private final Random random = new Random();
 
     public EvenBiasedDice(int numFaces) {
         this.numFaces = numFaces;
     }
 
     public int roll() {
-        Random random = new Random();
-
-        int res = random.nextInt(numFaces+1 - 1) + 1;
+        int res = random.nextInt(numFaces) + 1;
 
         while (res % 2 == 1) {
-            res = random.nextInt(numFaces+1 - 1) + 1;
+            res = random.nextInt(numFaces) + 1;
         }
 
         return res;
--- src/dice/FairDice.java
+++ src/dice/FairDice.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class FairDice implements Dice {
     private final int numFaces;
+    private final Random random = new Random();
 
     public FairDice(int numFaces) {
         this.numFaces = numFaces;
     }
 
     public int roll() {
-        Random random = new Random();
-        return random.nextInt(numFaces+1 - 1) + 1;
+        return random.nextInt(numFaces) + 1;
     }
 }
