7c3a83061cacbbd44d807343e215b5048873452c
pranit.k <pranit.kamarshi@cowrks.com>
2020-09-29 23:19:55 +0530

    Added Board and Snake

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Board {
+    private final int size;
+    private final Map<Integer, Snake> snakesMap = new HashMap<>();
+
+    public Board(int size) {
+        this.size = size;
+    }
+
+    public void addSnake(int start, int end) {
+        if (snakesMap.containsKey(start)) {
+            System.out.println("there is already a snake at position : " + start);
+            return;
+        }
+        snakesMap.put(start, new Snake(start, end));
+    }
+
+    public int move(int currentPosition, int delta) {
+        int newPosition = currentPosition + delta;
+
+        if (newPosition > size) {
+            System.out.println("dice roll causing new position to go beyond size of board " +
+                    "so not moving");
+
+            return currentPosition;
+        }
+
+        else if (snakesMap.containsKey(newPosition)) {
+            return snakesMap.get(newPosition).getEnd();
+        }
+
+        return newPosition;
+    }
 }
