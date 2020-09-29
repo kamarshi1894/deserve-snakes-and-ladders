c6012cd0cc0906136af5be062862759df240efcc
pranit.k <pranit.kamarshi@cowrks.com>
2020-09-30 00:42:25 +0530

    Added game class to encapsulate all the logic of playing a game. Also added the Driver class to test the game

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Driver {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter board size");
+        int boardSize = sc.nextInt();
+
+        System.out.println("Enter dice strategy : normal or crooked");
+        String diceStrategy = sc.next();
+
+        System.out.println("Enter number of dice faces");
+        int numDiceFaces = sc.nextInt();
+
+        System.out.println("Enter number of max moves");
+        int maxNumMoves = sc.nextInt();
+
+        Game game = new Game(boardSize, diceStrategy, numDiceFaces);
+
+        while (game.getNumberOfMoves() <= 10 && game.getPosition() < boardSize) {
+            System.out.println("Enter action :\n" +
+                               "1 to add a snake\n" +
+                               "2 to make a move");
+
+            String action = sc.next();
+
+            if(action.equals("1")) {
+                System.out.println("Enter start position of snake");
+                int start = sc.nextInt();
+                System.out.println("Enter end position of snake");
+                int end = sc.nextInt();
+
+                game.addSnake(start, end);
+            } else {
+                game.makeMove();
+            }
+        }
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import dice.Dice;
+import dice.EvenBiasedDice;
+import dice.FairDice;
 
 public class Game {
+    private final Board board;
+    private final Dice dice;
+
+    private int position = 0;
+    private int numberOfMoves = 0;
+
+    public Game(int boardSize, String diceStrategy, int numDiceFaces) {
+        this.board = new Board(boardSize);
+        this.dice = getDice(diceStrategy, numDiceFaces);
+    }
+
+    private Dice getDice(String strategy, int numDiceFaces) {
+        if (strategy.equals("crooked")) {
+            return new EvenBiasedDice(numDiceFaces);
+        } else {
+            return new FairDice(numDiceFaces);
+        }
+    }
+
+    public void makeMove() {
+        int tmp = position;
+        position = board.move(position, dice.roll());
+
+        if (position == board.getSize()) {
+            System.out.println("The end of the board has been reached. " +
+                    "The game has concluded");
+            return;
+        }
+
+        System.out.println(String.format("moved from %s to %s", tmp, position));
+
+        numberOfMoves++;
+    }
+
+    public void addSnake(int start, int end) {
+        board.addSnake(start, end);
+    }
+
+    public int getNumberOfMoves() {
+        return this.numberOfMoves;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
 }
